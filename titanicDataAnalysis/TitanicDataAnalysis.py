d00bbb9f20e48c9a1075bcc474b815a27de8a031
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-02-26 23:01:24 +0530

    drawing countplot of survived passengers

--- titanicDataAnalysis/TitanicDataAnalysis.py
+++ titanicDataAnalysis/TitanicDataAnalysis.py
@@ -8,3 +8,6 @@ dataset = pd.read_csv("tested.csv")
 print(dataset.head(10))
 
 print("number of passengers in original data:" + str(len(dataset.index)))
+
+sns.countplot(x="Survived", data=dataset)
+plt.show()
