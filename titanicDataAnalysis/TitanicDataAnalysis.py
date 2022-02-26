2928d95df19375892bbcaa05e965828e7544b63b
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-02-26 23:52:57 +0530

    Training and testing dataset

--- titanicDataAnalysis/TitanicDataAnalysis.py
+++ titanicDataAnalysis/TitanicDataAnalysis.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import math
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
